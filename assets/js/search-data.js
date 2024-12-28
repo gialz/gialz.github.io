dd60268c24b3745927c87a0d4a6abdeab5a33220
gialz <ginnaalzubaidi@icloud.com>
2024-12-28 17:04:27 +0000

    Deploying to gh-pages from @ gialz/gialz.github.io@e88cf264be22df3d195750e47406aad500fc0b08 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -439,8 +439,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-extra-mural-year-at-king-s-forensics-begins-sparkles-smile",
+          title: 'Extra Mural Year at King’s Forensics Begins! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
